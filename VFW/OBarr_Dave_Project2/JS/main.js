4f2365f555007b706cb25eb065010be021867e7c
Dave O'Barr <daveo@frontierlighting.com>
2011-09-06 21:15:18 -0400

    Finished coding JavaScript and will continue testing

---
+++ VFW/OBarr_Dave_Project2/JS/main.js
@@ -0,0 +1,59 @@
+/*	VFW Project 2
+	Term 1109
+	Dave O'Barr
+	September 8, 2011 */
+
+window.onload = getItems();
+
+function getItems(){
+
+if (localStorage.getItem('applampType')){
+
+var lampType = 		localStorage.getItem('applampType');
+var wattage = 		localStorage.getItem('appwattage');
+var yourName = 		localStorage.getItem('appyourName');
+var quantity = 		localStorage.getItem('appquantity');
+var delivery = 		localStorage.getItem('appdelivery');
+var dateRequired = 	localStorage.getItem('appdateRequired');
+var comments = 		localStorage.getItem('comments');
+
+var viewOrder = [
+	lampType,
+	wattage,
+	yourName,
+	quantity,
+	delivery,
+	dateRequired,
+	comments
+];	
+
+alert(viewOrder);	
+}
+};
+
+
+function storeItems(id) {
+
+var lampType = 		document.getElementById("lampType").value;
+var wattage = 		document.getElementById("wattage").value;
+var yourName = 		document.getElementById('yourName').value;
+var quantity = 		document.getElementById("quantity").value;
+var delivery = 		document.getElementById("delivery").value;
+var dateRequired = 	document.getElementById("dateRequired").value;
+var comments = 		document.getElementById("comments").value;
+		
+		localStorage.setItem('applampType',lampType);
+		localStorage.setItem('appwattage',wattage);
+		localStorage.setItem('appyourName',yourName);
+		localStorage.setItem('appquantity',quantity);
+		localStorage.setItem('appdelivery',delivery);
+		localStorage.setItem('appdateRequired',dateRequired);
+		localStorage.setItem('comments',comments);
+
+alert("Saved�");
+};
+
+
+
+
+
